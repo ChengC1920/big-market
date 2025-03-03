361db3a3894cef25333e133d70f117082b62690d
Ninetee <tanghuacheng2002@gmail.com>
2025-03-04 04:20:39 +0800

    feat: 用户参与抽奖活动库表设计

---
+++ big-market-infrastructure/src/main/java/cn/thc/infrastructure/persistent/po/RaffleActivityOrder.java
@@ -0,0 +1,66 @@
+package cn.thc.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author Ninetee Tang
+ * @description 抽奖活动单 持久化对象
+ * @create 2025/2/7 23:17
+ */
+@Data
+public class RaffleActivityOrder {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动名称
+     */
+    private String activityName;
+
+    /**
+     * 抽奖策略ID
+     */
+    private Long strategyId;
+
+    /**
+     * 订单ID
+     */
+    private String orderId;
+
+    /**
+     * 下单时间
+     */
+    private Date orderTime;
+
+    /**
+     * 订单状态（not_used、used、expire）
+     */
+    private String state;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
+
